79a60272682b82fc9588e335938d0358d17cdb1f
evelinaguzun <evelinaguzun8@gmail.com>
2025-10-16 13:32:46 +0300

    Initial commit - Spring Boot multi-env JDBC project

---
+++ multi-env-jdbc/src/main/java/ro/example/multi_env_jdbc/MultiEnvJdbcApplication.java
@@ -0,0 +1,13 @@
+package ro.example.multi_env_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiEnvJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiEnvJdbcApplication.class, args);
+	}
+
+}
---
+++ multi-env-jdbc/src/test/java/ro/example/multi_env_jdbc/MultiEnvJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.example.multi_env_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiEnvJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
